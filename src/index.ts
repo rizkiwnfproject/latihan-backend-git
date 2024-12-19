2c6542c121c2e24db7cc9bc845833a6f2cabd237
rizkiwnf <rizkiwahyunf2@gmail.com>
2024-12-19 11:55:36 +0700

    Add user authentication endpoints

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,20 @@ app.post('/login', (req: Request, res: Response) => {
     res.send('User logged in');
 });
 
+// Endpoint untuk register user
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk register user
+    res.send('User registered');
+});
+
+// Endpoint untuk login user
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logika untuk login user
+    res.send('User logged in');
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
