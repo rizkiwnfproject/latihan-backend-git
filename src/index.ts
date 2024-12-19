30d7a22d382a47b4e1b0e96a25d9d8275548d181
rizkiwnf <rizkiwahyunf2@gmail.com>
2024-12-19 11:56:11 +0700

    Add product management endpoints

--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,16 @@ app.get('/products', (req: Request, res: Response) => {
     // Logika untuk mendapatkan semua produk
     res.send('List of products');
 });
+
+// Endpoint untuk menambah produk
+app.post('/products', (req: Request, res: Response) => {
+    const { name, price } = req.body;
+    // Logika untuk menambah produk
+    res.send('Product added');
+});
+
+// Endpoint untuk mendapatkan semua produk
+app.get('/products', (req: Request, res: Response) => {
+    // Logika untuk mendapatkan semua produk
+    res.send('List of products');
+});
